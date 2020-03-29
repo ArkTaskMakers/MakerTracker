5b55ad25ba5a94fab432a22a8a4bb4f80b6f975c
Daniel Pollock <daniel.pollock@egov.com>
2020-03-29 13:34:40 -0500

    Adding dev auth0

---
+++ MakerTracker/Models/UserProfileViewModel.cs
@@ -0,0 +1,11 @@
+namespace MakerTracker.Models
+{
+    public class UserProfileViewModel
+    {
+        public string EmailAddress { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProfileImage { get; set; }
+    }
+}
