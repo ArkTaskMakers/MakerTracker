bcfec100cd4a8ec512f88ca2084f10d9ae9b90b3
Daniel Pollock <daniel.pollock@egov.com>
2020-04-01 16:38:42 -0500

    Build fix.

--- MakerTracker/Models/ReportMakerWork.cs
+++ MakerTracker/Models/ReportMakerWork.cs
@@ -12,15 +12,4 @@ namespace MakerTracker.Models
         public bool IsFinished { get; set; }
         public int ExpectedCount { get; set; }
     }
-
-    public class ReportMaker
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string ZipCode { get; set; }
-        public bool ProductsQueued { get; set; }
-        public int ProductsFinished { get; set; }
-    }
 }
