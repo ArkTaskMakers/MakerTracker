c75e8a87131b0ae57b5a81482f70f5ebcd3c9b38
Daniel Pollock <daniel.pollock@egov.com>
2020-04-06 23:00:30 -0500

    Adding dashboard controller and WhatIHave.cshtml

---
+++ MakerTracker/Models/WhatIHaveModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MakerTracker.Models
+{
+    public class WhatIHaveModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Amount { get; set; }
+        public string ProductImageUrl { get; set; }
+    }
+}
