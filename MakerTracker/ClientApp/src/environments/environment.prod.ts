9d9aa8c1cbb8a8fe056bc50084ab82e443c7f506
Daniel Pollock <daniel.pollock@egov.com>
2020-04-09 15:51:44 -0500

    Build fix. Gotta have the same settings in environment.prod.ts!

--- MakerTracker/ClientApp/src/environments/environment.prod.ts
+++ MakerTracker/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0_domain: 'dev-makertracker.auth0.com',
+  auth0_client_id: '5tPXXeVWdwpv3ftztWrO46vA38wlwe18',
+  auth0_audience: 'https://dev-makertracker.auth0.com/api/v2/'
 };
