3c08a65d283bd83b9ff31a9713a13a10cd9c419b
Daniel Pollock <daniel.pollock@egov.com>
2020-04-10 00:26:47 -0500

    Throwing up dashboard inventory numbers

--- MakerTracker/ClientApp/src/app/dashboard/dashboard.component.ts
+++ MakerTracker/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { BackendService } from '../backend/backend.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddInventoryComponent } from './dialogs/add-inventory/add-inventory.component';
 import { AddInventoryDto } from 'autogen/AddInventoryDto';
+import { InventoryProductSummaryDto } from 'autogen/InventoryProductSummaryDto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+
+export class DashboardComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -32,19 +34,26 @@ export class DashboardComponent {
       ];
     })
   );
+  inventorySummary: InventoryProductSummaryDto[];
 
   constructor(private breakpointObserver: BreakpointObserver, private backend: BackendService, public dialog: MatDialog) {
+  }
 
+  ngOnInit(): void {
+    this.refreshDashBoard();
+  }
+
+  private refreshDashBoard() {
+    this.backend.getInventorySummary().subscribe(res => this.inventorySummary = res);
   }
 
   openAddInventoryDialog(): void {
     const dialogRef = this.dialog.open(AddInventoryComponent, {
-      // width: '250px',
       data: <AddInventoryDto>{}
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed', result);
+      this.refreshDashBoard();
     });
   }
 
