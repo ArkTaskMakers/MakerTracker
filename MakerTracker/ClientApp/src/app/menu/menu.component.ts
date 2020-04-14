7147c04e7d8b70f991d73f28ae6117e7a145b7f0
Aaron Moore <mcaden@gmail.com>
2020-04-14 14:26:37 -0500

    correct menu styling

--- MakerTracker/ClientApp/src/app/menu/menu.component.ts
+++ MakerTracker/ClientApp/src/app/menu/menu.component.ts
@@ -15,7 +15,7 @@ import {
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
-  styleUrls: ["./menu.component.css"],
+  styleUrls: ["./menu.component.scss"],
 })
 export class MenuComponent implements OnInit, OnDestroy {
   private _subscriptions: Subscription[];
