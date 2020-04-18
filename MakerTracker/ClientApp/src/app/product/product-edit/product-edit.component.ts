bde8dcf83adca49016f9c90fcb4fb433c2f748ce
Aaron Moore <mcaden@gmail.com>
2020-04-17 23:29:21 -0500

    revised the landing page to approximate requested layout, added image tool on product admin that will convert an image on disk to a resized DataURL

--- MakerTracker/ClientApp/src/app/product/product-edit/product-edit.component.ts
+++ MakerTracker/ClientApp/src/app/product/product-edit/product-edit.component.ts
@@ -1,47 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
-import { ProductService } from '../product.service';
-import { Product } from '../product';
-import { map, switchMap } from 'rxjs/operators';
-import { of } from 'rxjs';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
+import { ProductService } from "../product.service";
+import { Product } from "../product";
+import { map, switchMap } from "rxjs/operators";
+import { of } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-product-edit',
-  templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.scss']
+  selector: "app-product-edit",
+  templateUrl: "./product-edit.component.html",
+  styleUrls: ["./product-edit.component.scss"],
 })
 export class ProductEditComponent implements OnInit {
-
   id: string;
   product: Product;
   feedback: any = {};
+  src: string = "";
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private _snackBar: MatSnackBar,
-    private productService: ProductService) {
-  }
+    private productService: ProductService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
-    this
-      .route
-      .params
+    this.route.params
       .pipe(
-        map(p => p.id),
-        switchMap(id => {
-          if (id === 'new') { return of(new Product()); }
+        map((p) => p.id),
+        switchMap((id) => {
+          if (id === "new") {
+            return of(new Product());
+          }
           return this.productService.findById(id);
         })
       )
-      .subscribe(product => {
-        this.product = product;
-        this.feedback = {};
-      },
-        err => {
-          this._snackBar.open('Error loading', null, {
+      .subscribe(
+        (product) => {
+          this.product = product;
+          this.feedback = {};
+        },
+        (err) => {
+          this._snackBar.open("Error loading", null, {
             duration: 2000,
           });
         }
@@ -50,15 +52,15 @@ export class ProductEditComponent implements OnInit {
 
   save() {
     this.productService.save(this.product).subscribe(
-      product => {
+      (product) => {
         this.product = product;
-        this._snackBar.open('Save was successful!', null, {
+        this._snackBar.open("Save was successful!", null, {
           duration: 2000,
         });
-        this.router.navigate(['/products']);
+        this.router.navigate(["/products"]);
       },
-      err => {
-        this._snackBar.open('Error Saving', null, {
+      (err) => {
+        this._snackBar.open("Error Saving", null, {
           duration: 2000,
         });
       }
@@ -66,6 +68,49 @@ export class ProductEditComponent implements OnInit {
   }
 
   cancel() {
-    this.router.navigate(['/products']);
+    this.router.navigate(["/products"]);
+  }
+
+  onFileChange(event) {
+    let reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.compressImage(reader.result, 64, 64).then(
+          (compressed: string) => {
+            this.src = compressed;
+            this.product.imageUrl = compressed;
+          },
+          (err) => {
+            this._snackBar.open('Invalid image', null, {
+              duration: 2000,
+            });
+          }
+        );
+
+        // need to run CD since file load runs outside of zone
+        this.cd.markForCheck();
+      };
+    }
+  }
+
+  compressImage(src, newX, newY) {
+    return new Promise((res, rej) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => {
+        const elem = document.createElement("canvas");
+        elem.width = newX;
+        elem.height = newY;
+        const ctx = elem.getContext("2d");
+        ctx.drawImage(img, 0, 0, newX, newY);
+        const data = ctx.canvas.toDataURL("image/jpeg", 0.75);
+        res(data);
+      };
+      img.onerror = (error) => rej(error);
+    });
   }
 }
