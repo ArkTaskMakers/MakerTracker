fde485fdee6112e551c3dd400967b294cae3be52
Daniel Pollock <daniel.pollock@egov.com>
2020-05-20 22:14:12 -0500

    Adding admin guard.

---
+++ MakerTracker/ClientApp/src/app/guards/admin.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private _authService: AuthService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return this._authService.hasRole("Admin");
+  }
+
+}
--- MakerTracker/ClientApp/src/app/lookup-manager/lookup.routes.ts
+++ MakerTracker/ClientApp/src/app/lookup-manager/lookup.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
+import { AdminGuard } from '../guards/admin.guard';
 import { LookupEditorComponent } from './edit/lookup-editor.component';
 import { LookupListComponent } from './list/lookup-list.component';
 
 export const LOOKUP_ROUTES: Routes = [
   {
     path: 'admin/:model',
-    component: LookupListComponent
+    component: LookupListComponent,
+    canActivate: [AdminGuard]
   },
   {
     path: 'admin/:model/:id',
-    component: LookupEditorComponent
+    component: LookupEditorComponent,
+    canActivate: [AdminGuard]
   }
 ];
