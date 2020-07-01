295ccbfa83ba8fa7c396d1025f9405ff3163f69d
Daniel Pollock <daniel.pollock@egov.com>
2020-07-01 16:14:43 -0500

    updating auth0 prod keys

--- MakerTracker/ClientApp/src/environments/environment.prod.ts
+++ MakerTracker/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  auth0_domain: 'dev-makertracker.auth0.com',
-  auth0_client_id: '5tPXXeVWdwpv3ftztWrO46vA38wlwe18',
-  auth0_audience: 'https://dev-makertracker.auth0.com/api/v2/'
+  auth0_domain: 'makertracker.auth0.com',
+  auth0_client_id: 'rAtUgwA3jbFHoyEjKUcwGA60ZmKpCPfX',
+  auth0_audience: 'https://makertracker.auth0.com/api/v2/'
 };
